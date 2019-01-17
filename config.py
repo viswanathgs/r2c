4ad5cdb1ad2edc2c45e15d3b601bdde9a4de6b9a
Viswanath Sivakumar <viswanath@fb.com>
2019-01-17 12:13:22 -0800

    Update dataset paths

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 USE_IMAGENET_PRETRAINED = True # otherwise use detectron, but that doesnt seem to work?!?
 
 # Change these to match where your annotations and images are
-VCR_IMAGES_DIR = '/home/rowan/datasets2/vswag'
-VCR_ANNOTS_DIR = '/home/rowan/code/vswagmodels/data'
+VCR_IMAGES_DIR = '/datasets01/vcr1/011619/vcr1images'
+VCR_ANNOTS_DIR = '/datasets01/vcr1/011619/data'
