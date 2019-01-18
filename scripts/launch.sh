bbcc92464cd50a0c60fc0fb16096d6bab73b662f
Viswanath Sivakumar <viswanath@fb.com>
2019-01-18 02:30:51 -0800

    Fix launcher scripts

--- scripts/launch.sh
+++ scripts/launch.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+# Usage: sbatch launch.sh <BASE_DIR_TO_R2C_SOURCE>
+
 #SBATCH --job-name=r2c
 #SBATCH --output=/checkpoint/%u/r2c/%j/stdout.log
 #SBATCH --error=/checkpoint/%u/r2c/%j/stderr.log
@@ -8,8 +10,10 @@
 #SBATCH --ntasks-per-node=1
 #SBATCH --gres=gpu:8
 #SBATCH --cpus-per-task=32
-#SBATCH --time=12:00:00
+#SBATCH --time=72:00:00
 #SBATCH --open-mode=append
+
+# TODO (viswanath): Reeval cpu, gpu and time
 
 . /usr/share/modules/init/sh
 
@@ -23,6 +27,12 @@ module load anaconda3/5.0.1
 
 source activate /private/home/"$USER"/.conda/envs/vcr
 
-BASEDIR=$(dirname $0)/..
+BASEDIR=${2:-"/private/home/$USER/projects/r2c"}
+SOURCE="$BASEDIR"/models/train.py
+PARAMS="$BASEDIR"/models/multiatt/default.json
+CHECKPOINT_DIR=/checkpoint/$USER/r2c/$SLURM_JOB_ID
 
-srun --label "$BASEDIR"/scripts/wrapper.sh
+echo "Running Job $SLURMD_NODENAME $SLURM_JOB_ID $CUDA_VISIBLE_DEVICES"
+echo "Checkpoint dir: $CHECKPOINT_DIR"
+
+srun --label python $SOURCE -params $PARAMS -folder $CHECKPOINT_DIR -no_tqdm
